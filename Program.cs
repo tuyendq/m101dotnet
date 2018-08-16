21dc67cb76a87a6d4edcc199f8ed73c27e6ce5e2
Tuyen Dang <tuyendq@gmail.com>
2018-08-16 23:33:22 +0700

    asusrog: dotnet add package MongoDB.Driver

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace M101DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            MainAsync(args).GetAwaiter().GetResult();
+            System.Console.WriteLine();
+            System.Console.WriteLine("Press Enter");
+            System.Console.WriteLine();
+        }
+
+        static async Task MainAsync(string[] agrs)
+        {
+            // await Task.Delay(500);
+            var client = new MongoClient();
+            
+        }
+    }
+}
